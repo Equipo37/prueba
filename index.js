5b552c4a933433bf4493e9c32485fcf9d31435e9
Gustavo Torres <betogus2009@gmail.com>
2023-08-13 00:07:38 -0300

    corrigio el port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const port = PORT || 8080
+const port = 8080
 app.listen(port, () => console.log("Escuchando en puerto " + port))
 app.get("/", (req, res) => {
     res.json("hola")
